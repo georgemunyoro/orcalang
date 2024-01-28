163e693bfa645115ffe95dbd8e766198e8eae740
George Munyoro <george@munyoro.com>
2024-01-28 03:54:25 +0200

    fix: better naming convention for approvaltests snapshot file naming

--- test/snapshot.cpp
+++ test/snapshot.cpp
@@ -13,43 +13,49 @@ namespace fs = std::filesystem;
 class Fixture
     : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {};
 
+auto createName(const std::string &stage, const std::string &testKind,
+                const std::string &testCaseName) {
+  return ApprovalTests::TemplatedCustomNamer::create(
+      "{TestSourceDirectory}/{ApprovalsSubdirectory}/"
+      "s" +
+      stage + "_" + testKind + "_" + testCaseName +
+      ".{ApprovedOrReceived}.{FileExtension}");
+}
+
 TEST_P(Fixture, ast_typed) {
   std::string stage, testCaseName;
   std::tie(stage, testCaseName) = GetParam();
 
-  {
-    auto section =
-        ApprovalTests::NamerFactory::appendToOutputFilename(testCaseName);
-    auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
-                                          "/valid/" + testCaseName + ".orca");
-    ApprovalTests::Approvals::verify(astStrings.astStringWithTypes);
-  }
+  auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
+                                        "/valid/" + testCaseName + ".orca");
+  ApprovalTests::Approvals::verify(
+      astStrings.astStringWithTypes,
+      ApprovalTests::Options().withNamer(
+          createName(stage, "ast_typed", testCaseName)));
 }
 
 TEST_P(Fixture, ast_untyped) {
   std::string stage, testCaseName;
   std::tie(stage, testCaseName) = GetParam();
 
-  {
-    auto section =
-        ApprovalTests::NamerFactory::appendToOutputFilename(testCaseName);
-    auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
-                                          "/valid/" + testCaseName + ".orca");
-    ApprovalTests::Approvals::verify(astStrings.astStringWithoutTypes);
-  }
+  auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
+                                        "/valid/" + testCaseName + ".orca");
+  ApprovalTests::Approvals::verify(
+      astStrings.astStringWithoutTypes,
+      ApprovalTests::Options().withNamer(
+          createName(stage, "ast_untyped", testCaseName)));
 }
 
 TEST_P(Fixture, llvm_ir) {
   std::string stage, testCaseName;
   std::tie(stage, testCaseName) = GetParam();
 
-  {
-    auto section =
-        ApprovalTests::NamerFactory::appendToOutputFilename(testCaseName);
-    auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
-                                          "/valid/" + testCaseName + ".orca");
-    ApprovalTests::Approvals::verify(astStrings.mainFunctionLLVMIr);
-  }
+  auto astStrings = runAndGetAstStrings("../test/cases/stage" + stage +
+                                        "/valid/" + testCaseName + ".orca");
+  ApprovalTests::Approvals::verify(
+      astStrings.mainFunctionLLVMIr,
+      ApprovalTests::Options().withNamer(
+          createName(stage, "llvm_ir", testCaseName)));
 }
 
 auto GetData(const std::string &stage, const std::string &type) {
